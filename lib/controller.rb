951d3d82b9f5af85bf3bc8ea6eb29e8568ef9483
root <root@bt.foo.org>
2012-12-17 00:05:56 -0600

    email address enum added

--- lib/controller.rb
+++ lib/controller.rb
@@ -19,6 +19,8 @@ def run_command(command)
 		return :quit
 	when 'help'
 		puts help
+	when 'emails'
+		launch_enum_emails
 	when 'sitemap'
 		launch_sitemap_creation
 	when 'spider'
@@ -31,6 +33,32 @@ def run_command(command)
 		system("clear")
 	else
 		puts "[-] Invalid Option, try 'help'"
+	end
+end
+
+def launch_enum_emails
+	print "[.] Target URL: "
+	# Take in a URL from the user and spider it
+	target = gets.chomp
+	if clean_url(target)
+		begin
+			Anemone.crawl(target) do |anemone|
+				anemone.on_every_page do |page|
+					# enum email addresses
+					emails = http_get("#{page.url}")
+					unless emails == nil
+						emails.each do |email|
+							puts "#{page.url}\t#{email}"
+						end
+					end
+				end
+			end
+		catch
+			puts "An Error has occured while crawling the url"
+		end
+	else
+		puts "[-] Error: must provide an absolute URL 'http://www...'"
+		console_loop
 	end
 end
 
@@ -94,6 +122,27 @@ def enum_webserver
 	end  
 end
 
+def http_get(url)
+	# ignore if https url
+	if url =~ /https/
+		puts "This tool does not support HTTPS at this time"
+
+		# code to handle https
+		return
+	end
+
+	uri = URI.parse(url)
+	body = Net::HTTP.get(uri)
+
+	emails = []
+	if body =~ /\b[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,4}\b/
+		emails << body.match(/\b[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,4}\b/)
+		unless emails == nil
+			return emails
+		end
+	end
+end
+
 def help
 	# This is the help menu
 	help_string = "\n\n  --Commands--"
@@ -101,6 +150,7 @@ def help
 	help_string += "help\t-\tDisplays this help screen\r\n"
 	help_string += "sitemap\t-\tCreates a Sitemap of targets url\r\n"
 	help_string += "spider\t-\tSpiders a specified target website\r\n"
+	help_string += "emails\t-\tEnumeates email addresses within a url\r\n"
 	help_string += "enum\t-\tRuns web enumeration modules against a specified URL\n"
 	help_string += "hosts\t-\tDisplay information about scanned hosts\n"
 	help_string += "quit\t-\tExits the applicaiton\r\n"
